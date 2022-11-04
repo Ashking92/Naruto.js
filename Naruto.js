fe995fdcb745bebd781424639a1ea41aa4890bfe
Ash king <88268317+Ashking92@users.noreply.github.com>
2022-11-04 17:51:24 +0530

    Create Naruto.js

---
+++ Naruto.js
@@ -0,0 +1,8 @@
+{ "CREATOR" : "ASH KING",
+   "result" : 
+["https://i.imgur.com/dtmFXd8.mp4",
+"https://i.imgur.com/sIhrW6W.mp4", 
+"https://i.imgur.com/X5l475S.mp4",
+"https://i.imgur.com/TiEGMsr.mp4", 
+]
+}
